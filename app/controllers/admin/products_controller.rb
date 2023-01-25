e1d282ad71d89abbdb8384bfa1003221ebec5037
WesleyMatthee <wrmatthee@gmail.com>
2023-01-25 23:41:33 +0000

    Add product count and product category in Admnin Dashboard

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ProductsController < ApplicationController
-http_basic_authenticate_with name: ENV["NAME"], password: ENV["PASSWORD"], only: :index
+http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"], only: :index
 
 
   def index
