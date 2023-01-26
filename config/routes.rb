bee37d30b0d229818e4fc41ddac7702f55d138a3
WesleyMatthee <wrmatthee@gmail.com>
2023-01-26 21:51:23 +0000

    Add Ul and Il to html signin and login. Reorder routing for singin and login in /config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
+ get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
 
   resources :about, only: [:index]
+  
   resources :products, only: [:index, :show]
-  resources :categories, only: [:index, :show]
-
+  resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
-
-    get '/login' => 'sessions#new'
-    post '/login' => 'sessions#create'
-    get '/logout' => 'sessions#destroy'
-
-    get '/signup' => 'users#new'
-    post '/users' => 'users#create'
   end
 
   resources :orders, only: [:create, :show]
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show]
   end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
